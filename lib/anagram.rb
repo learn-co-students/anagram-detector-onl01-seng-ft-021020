a43164e6a392ab381df39894f3a656677223128d
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-22 21:21:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word) 
+    word_split = word.split("")
+    @word = word_split.sort
+  end 
+  
+  def match(words) 
+    matching_anagram = []
+    words.each do |word| 
+     word_split = word.split("")
+      if @word == word_split.sort 
+        matching_anagram << word 
+      end 
+    end
+    matching_anagram
+  end 
+
+end 
