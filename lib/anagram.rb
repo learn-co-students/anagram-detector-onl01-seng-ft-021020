13d43222f0f6072cda201bee6c84eae98f292d20
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-24 01:42:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :w
+  
+  def initialize(w)
+    @w = w
+  end
+  
+  def match(w_array)
+    w_array.select do |w|
+      w.split("").sort == @w.split("").sort
+    end
+  end
+end
